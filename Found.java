cba15bd7c5d3fe1a5e43df40793ae9eb90286a84
Nivetha003 <niviragu2k5@gmail.com>
2025-12-15 18:54:01 +0530

    Add files via upload

---
+++ Found.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Found
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int num = sc.nextInt();
+	    int digit = sc.nextInt();
+	    while(num>0){
+	        if(num%10==digit){
+	            System.out.println("digit found");
+	            return;
+	        }
+	        num = num/10;
+	    }
+	   
+	    System.out.println("not found");
+	   
+	}
+}
